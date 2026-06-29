5016e9783c84c5fccb52c384b3ffa5e76f8e6599
asph1xslvts <57061950+asph1xslvts@users.noreply.github.com>
2026-06-30 00:49:13 +0300

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -11,7 +11,8 @@ local LIBS = {
     {"https://raw.githubusercontent.com/IgorTimofeev/Color/master/Color.lua",                 "/usr/lib/color.lua"},
     {"https://raw.githubusercontent.com/IgorTimofeev/DoubleBuffering/master/DoubleBuffering.lua", "/usr/lib/doubleBuffering.lua"},
     {"https://raw.githubusercontent.com/IgorTimofeev/Image/master/Image.lua",                 "/usr/lib/image.lua"},
-    {"https://raw.githubusercontent.com/IgorTimofeev/Image/master/OCIF.lua",                 "/usr/lib/OCIF.lua"},
+    {"https://raw.githubusercontent.com/IgorTimofeev/Image/master/OCIF.lua", "/lib/FormatModules/OCIF.lua"},
+    {"https://raw.githubusercontent.com/IgorTimofeev/Image/master/OCIF.lua", "/usr/lib/FormatModules/OCIF.lua"},
 }
 
 -- fayly kazino i ikonki s tvoego repozitoriya {na_github, kuda}
